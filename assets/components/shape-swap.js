43c307b1fbace5876e391f45db5975795f57c4da
Marc <zappythepro@gmail.com>
2017-07-12 10:33:59 +0100

    fixed sizes

--- assets/components/shape-swap.js
+++ assets/components/shape-swap.js
@@ -11,7 +11,12 @@ AFRAME.registerComponent('shape-swap', {
     var el = this.el;
 
     el.addEventListener(data.on, function () {
-      data.target.setAttribute('geometry', {primitive: data.shape, size: data.size});
+      var size = data.size.split(" ");
+      var width = size[0];
+      var height = size[1];
+      var depth = size[2];
+      
+      data.target.setAttribute('geometry', {primitive: data.shape, width: width, depth: depth, height: height});
     });
   }
 });
